1c60843ffca55d1316f9b3ea83a899729f2dee5b
Andreixzc <andrei.massaini@hotmail.com>
2022-07-01 15:28:25 -0300

    Login

---
+++ src/models/Usuario.js
@@ -0,0 +1,9 @@
+class Usuario {
+    constructor({id,email,login,nome,senha}) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.nome = nome;
+        this.senha = senha;
+    }
+  }
--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,18 +1,46 @@
-import { UserController } from "../controllers/UserController.js";
+// import { UserController } from "../controllers/UserController.js";
 
-const formLogin = document.getElementById("form_login_usuario");
-formLogin.addEventListener("submit", coletaDados);
+// const formLogin = document.getElementById("form_login_usuario");
+// formLogin.addEventListener("submit", coletaDados);
 
-async function coletaDados(event) {
-  event.preventDefault();
+// async function coletaDados(event) {
+//   event.preventDefault();
 
-  const formDados = new FormData(event.target);
-  const novoLogin = Object.fromEntries(formDados);
-  formLogin.reset();
+//   const formDados = new FormData(event.target);
+//   const novoLogin = Object.fromEntries(formDados);
+//   formLogin.reset();
 
-  const statusLogin = await UserController.logarUsuario(novoLogin);
-  console.log(statusLogin);
-  if (statusLogin.token) {
-    location.href = "/src/Delicious/index.html";
-  }
+//   const statusLogin = await UserController.logarUsuario(novoLogin);
+//   console.log(statusLogin);
+//   if (statusLogin.token) {
+//     location.href = "/src/Delicious/index.html";
+//   }
+// }
+
+const urlInsertUser ="http://localhost:6789/usuario/login";
+const formCadastrar = document.getElementById("form_login_usuario");
+formCadastrar.addEventListener("submit", validar);
+async function validar(event) {
+    event.preventDefault();
+    const formDados = new FormData(event.target);
+    const novoUsuario = JSON.stringify(Object.fromEntries(formDados));      
+    fazPost(urlInsertUser,novoUsuario);
+    // window.location = "/login.html";
 }
+function fazPost(url,body) {
+    let request = new XMLHttpRequest();
+    request.open("POST",url,true);
+    request.send(body);
+    request.onload = function(event){
+      let usuario = new Usuario(JSON.parse(request.response));
+      console.log(usuario);
+      if (usuario.id != 0) {
+        window.location = "/src/Delicious/index.html";
+      } else {
+        window.alert("Credenciais Invalidas");
+      }
+      
+    }
+    
+}
+
